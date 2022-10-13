f8806160b40ed2749e44db412368661a66403159
liquidsec <paul.mueller08@gmail.com>
2022-10-13 15:22:06 -0400

    Added peoplesoft PS_TOKEN module

---
+++ tests/peoplesoft_test.py
@@ -0,0 +1,28 @@
+import sys
+import os
+
+SCRIPT_DIR = os.path.dirname(os.path.abspath(__file__))
+sys.path.append(os.path.dirname(SCRIPT_DIR))
+
+from popsecrets import Peoplesoft_PSToken
+
+tests = [
+    (
+        "popsecrets",
+        "qAAAAAQDAgEBAAAAvAIAAAAAAAAsAAAABABTaGRyAk4AdQg4AC4AMQAwABRhZGwcBykRPNQv++kTK0KePPqVVGgAAAAFAFNkYXRhXHicHYc7DkBQAATnIUqVa3jxLRzApxJBrxA18bmdw1l2k9nZG/Bcxxjt4/An3NnYOVlZOMRL7ld0NAQ9IzUTMy0DeUpMqkYkso+ZGFNiKbRW//Pyb0Guzwtozw4Q",
+    ),
+    (
+        "popsecrets",
+        "qAAAAAQDAgEBAAAAvAIAAAAAAAAsAAAABABTaGRyAk4AdQg4AC4AMQAwABR4TTHt0Umy1Cy8qZgJHJLdV/U0FWgAAAAFAFNkYXRhXHicHYc7DkBQAATnIUqVa3jxLRzApxJBrxA18bmdw1l2k9nZG/Bcxxjt4/An3NnYOVlZOMRL7ld0NAQ9IzUTMy0DeUpMqkYkso+ZGFNiKbRW//Pyb0Guzwtozw4Q",
+    ),
+]
+
+
+def test_peoplesoft():
+    for test in tests:
+
+        x = Peoplesoft_PSToken(test[1])
+        found_key = x.check_secret()
+        assert found_key == True
+        assert x.output_parameters["username"] == test[0]
+        assert x.output_parameters["PS_TOKEN_password"] != None
