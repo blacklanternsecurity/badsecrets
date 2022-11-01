4b6933d727390268a3a8da58d9d32425d543dc27
liquidsec <paul.mueller08@gmail.com>
2022-11-01 12:19:03 -0400

    tests tests tests

--- tests/peoplesoft_pstoken_test.py
+++ tests/peoplesoft_pstoken_test.py
@@ -26,8 +26,17 @@ def test_peoplesoft():
 
 
 def test_peoplesoft_negative():
+
+    # Token doesn't decode properly
     x = Peoplesoft_PSToken()
     found_key = x.check_secret(
         "qAAAAAQDAgEBAAAAvAIAAAAAAAAsAAAABABTaGRyAk4AdQg4AC4AMQAwABT5mYioG/i325GsBHHNyDIM+9yf1GgAAAAFAFNkYXRhXHicHYfJDUBQAESfJY5O2iDWgwIsJxHcxdaApTvFGX8mefPmAVzHtizta2MSrCzsXBxsnOIt9yo6GvyekZqJmZaBADSECRETS2c9MjCmJKLSR/u+laUGuzwdaGw3o"
+    )
+    assert not found_key
+
+    # Proper token with non-matching key
+    x = Peoplesoft_PSToken()
+    found_key = x.check_secret(
+        "owAAAAQDAgEBAAAAvAIAAAAAAAAsAAAABABTaGRyAk4Abwg4AC4AMQAwABRSZ/l0LBytKLW6TUnZ9GVFdgtqjWMAAAAFAFNkYXRhV3icJYhLDkAwFEVPSwztRFO0gw4lvgMimFuCDVqc17o3OffzAHmmlZJ8NUnlzklHz8rCRjEIpv8dubiZOXANlkZcUUuLbIWWgMFLGtmxh2SPk80Hk6gLyA=="
     )
     assert not found_key
