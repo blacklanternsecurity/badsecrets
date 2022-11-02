51ca8a2dcd3e928f13b05de11cb511ce235c6eb0
liquidsec <paul.mueller08@gmail.com>
2022-11-02 15:59:52 -0400

    bug fix in generic_jwt causing wrong key name to be reported

--- badsecrets/modules/generic_jwt.py
+++ badsecrets/modules/generic_jwt.py
@@ -40,10 +40,9 @@ class Generic_JWT(BadsecretsBase):
             for l in self.load_resource("jwt_rsakeys_public.txt"):
                 private_key_name = l.split(":")[0]
                 public_key = l.split(":")[1]
-            private_key_name = private_key_name
-            public_key = b"-----BEGIN PUBLIC KEY-----\nMIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEA6cs10W3XKnr1MDoO0Ngf\nYEixdQy5e3m/E4POPC5t6yyc/eZZayytrA6CfaZXBKnYU4YKD06sJULj30qw/TJJ\nwphhb2a5s3sjXejL4KW2WTdP6F+DbSaokzvKVdaZ97GnLtiei8n6gnSE1xSsJ15+\nd9JHImekuW/ggksVbI26UTiXvfv7LUJ8ntt6wG1UQHWOvYbG81TTpZjItvZsYu1t\npekjNpOwCsIbO//S1JOiSgpuKp7HwCnQwABNEWyMuIAMlymMyocbTdQHcClogZC9\nbwokxTPZGmD9xZ+meaeVD5HONqASIJ1tOoFGsnwwwlEhwsul0FRs7qehuhJmKE5Z\nbwIDAQAB\n-----END PUBLIC KEY-----"
-            r = self.jwtVerify(JWT, public_key, algorithm)
-            if r:
-                return {"jwt_private_key_index": private_key_name, "jwt_headers": jwt_headers, "jwt_data": r}
+                public_key = b"-----BEGIN PUBLIC KEY-----\nMIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEA6cs10W3XKnr1MDoO0Ngf\nYEixdQy5e3m/E4POPC5t6yyc/eZZayytrA6CfaZXBKnYU4YKD06sJULj30qw/TJJ\nwphhb2a5s3sjXejL4KW2WTdP6F+DbSaokzvKVdaZ97GnLtiei8n6gnSE1xSsJ15+\nd9JHImekuW/ggksVbI26UTiXvfv7LUJ8ntt6wG1UQHWOvYbG81TTpZjItvZsYu1t\npekjNpOwCsIbO//S1JOiSgpuKp7HwCnQwABNEWyMuIAMlymMyocbTdQHcClogZC9\nbwokxTPZGmD9xZ+meaeVD5HONqASIJ1tOoFGsnwwwlEhwsul0FRs7qehuhJmKE5Z\nbwIDAQAB\n-----END PUBLIC KEY-----"
+                r = self.jwtVerify(JWT, public_key, algorithm)
+                if r:
+                    return {"jwt_private_key_index": private_key_name, "jwt_headers": jwt_headers, "jwt_data": r}
 
         return None
