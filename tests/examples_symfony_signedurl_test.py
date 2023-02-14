ee1dadb53cc46c8da8eab03a4a566d45aabbdd47
liquidsec <paul.mueller08@gmail.com>
2023-02-13 20:17:18 -0500

    fix test bug

--- tests/examples_symfony_signedurl_test.py
+++ tests/examples_symfony_signedurl_test.py
@@ -18,7 +18,7 @@ def test_symfony_url_not_up(monkeypatch, capsys):
     with requests_mock.Mocker() as m:
         # URL is down - handled correctly
 
-        m.get(f"http://notreal.com/", exc=requests.exceptions.ConnectTimeout)
+        m.get(f"http://notreal.com/_fragment", exc=requests.exceptions.ConnectTimeout)
         monkeypatch.setattr("sys.argv", ["python", "--url", "http://notreal.com"])
         symfony_knownkey.main()
         captured = capsys.readouterr()
